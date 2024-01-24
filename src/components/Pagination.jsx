8de4698293cf2f2ccabe208ea3d8321e83572d69
Guga <gugakobakhidze18@gmail.com>
2024-01-25 01:16:39 +0400

    adding changes

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,7 +3,7 @@ import Stack from "@mui/material/Stack";
 import Pagination from "@mui/material/Pagination";
 import useScrollToTop from "../hooks/useScrollToTop";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart, faCircle } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { Link, useParams } from "react-router-dom";
 import { FavoritePage, ProductGrid, ProductsListTop } from "../pages/AllPages";
 import { useProductsContext } from "../contexts/ProductsContextProvider";
@@ -13,7 +13,7 @@ import { useTranslation } from "react-i18next";
 const ProductList = ({ products, productsPerPage, category }) => {
   const { setMainPhoto, currentPage, setCurrentPage, favorites } =
     useProductsContext();
-    const {t} = useTranslation()
+  const { t } = useTranslation();
   const startIndex = (currentPage - 1) * productsPerPage;
   const endIndex = startIndex + productsPerPage;
   const currentProducts = products.slice(startIndex, endIndex);
@@ -34,81 +34,73 @@ const ProductList = ({ products, productsPerPage, category }) => {
   };
 
   const favoritesIds = favorites ? favorites.map((product) => product.id) : [];
-  const isPaginationNeeded = products.length <= productsPerPage
+  const isPaginationNeeded = products.length <= productsPerPage;
 
   return (
     <div>
-
-{products.length > 0 ? 
-    
-    <div>
-      <ProductsListTop>
-        <h2 className="new-collection-title">{category}</h2>
-        <Filter />
-      </ProductsListTop>
-      <ProductGrid>
-        {currentProducts.map((product) => (
-          <div
-            className="product-container"
-            key={product.id}
-            onClick={handleClick}
-          >
-            <Link
-              style={{ color: "black" }}
-              to={`/${product.category}/products/${product.id}`}
-            >
-              <div className="product-img-and-icon">
-                <img
-                  className="product-image"
-                  src={product.image}
-                  alt=""
-                  onLoad={() => setPhotoLoaded(true)}
-                />
-                {favoritesIds.includes(product.id) && (
-                  <div className="favorite-icon-circle">
-                    <FontAwesomeIcon
-                      size="lg"
-                      icon={faHeart}
+      {products.length > 0 ? (
+        <div>
+          <ProductsListTop>
+            <h2 className="new-collection-title">{category}</h2>
+            <Filter />
+          </ProductsListTop>
+          <ProductGrid>
+            {currentProducts.map((product) => (
+              <div
+                className="product-container"
+                key={product.id}
+                onClick={handleClick}
+              >
+                <Link
+                  style={{ color: "black" }}
+                  to={`/${product.category}/products/${product.id}`}
+                >
+                  <div className="product-img-and-icon">
+                    <img
+                      className="product-image"
+                      src={product.image}
+                      alt=""
+                      onLoad={() => setPhotoLoaded(true)}
                     />
+                    {favoritesIds.includes(product.id) && (
+                      <div className="favorite-icon-circle">
+                        <FontAwesomeIcon size="lg" icon={faHeart} />
+                      </div>
+                    )}
                   </div>
-                )}
+                  {photoLoaded && (
+                    <h2 className="product-name">{product.name}</h2>
+                  )}
+                  {photoLoaded && (
+                    <div className="product-color">
+                      <h3 className="product-price">{`${product.price}₾`}</h3>
+                    </div>
+                  )}
+                </Link>
               </div>
-              {photoLoaded && <h2 className="product-name">{product.name}</h2>}
-              {photoLoaded && (
-                <div className="product-color">
-                  <h3 className="product-price">{`${product.price}₾`}</h3>
-                  <FontAwesomeIcon
-                    icon={faCircle}
-                    style={{ color: `${product.color}` }}
-                  />
-                </div>
-              )}
+            ))}
+          </ProductGrid>
+          {photoLoaded && !isPaginationNeeded && (
+            <Stack spacing={2}>
+              <Pagination
+                className="pagination"
+                count={Math.ceil(products.length / productsPerPage)}
+                page={currentPage}
+                onChange={handlePageChange}
+              />
+            </Stack>
+          )}
+        </div>
+      ) : (
+        <FavoritePage>
+          <div className="favorites-no-products">
+            <h2 className="favorites-text">{t("not found")}</h2>
+            <Link className="favorites-back-button" to="/">
+              {t("back to main")}
             </Link>
           </div>
-        ))}
-      </ProductGrid>
-      {photoLoaded && !isPaginationNeeded &&(
-        <Stack spacing={2}>
-          <Pagination
-            className="pagination"
-            count={Math.ceil(products.length / productsPerPage)}
-            page={currentPage}
-            onChange={handlePageChange}
-          />
-        </Stack>
+        </FavoritePage>
       )}
-    </div> :
-    (
-      <FavoritePage>
-      <div className="favorites-no-products">
-        <h2 className="favorites-text">{t("not found")}</h2>
-        <Link className="favorites-back-button" to="/">
-          {t("back to main")}
-        </Link>
-      </div>
-      </FavoritePage>
-    )}
-    
     </div>
   );
 };
